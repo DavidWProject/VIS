a2dffdd6932b16fd3216af2db0ec21313418f941
David Wong <davidwong@Davids-MacBook-Pro-5.local>
2018-11-28 17:49:30 -0500

    fixing https

--- server.js
+++ server.js
@@ -5,9 +5,6 @@ const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const http = require('http');
-const enforce = require('express-sslify');
-
 // Configure body parsing for AJAX requests
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -17,9 +14,9 @@ if (process.env.NODE_ENV === "production") {
 }
 
 // Add routes, both API and view
-app.use(enforce.HTTPS(routes));
+app.use(routes);
 
-// // Connect to the Mongo DB
+// Connect to the Mongo DB
 // mongoose.connect(
 //   process.env.MONGODB_URI || "mongodb://localhost/googlebooks",
 //   {
@@ -32,7 +29,3 @@ app.use(enforce.HTTPS(routes));
 app.listen(PORT, () =>
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`)
 );
-
-// http.createServer(app).listen(app.get('port'), function() {
-//   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`)
-// });
